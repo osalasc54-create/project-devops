3433926ac53dbe92f8926e16a71e036cc81f6038
Omar Salas <osalasc54@gmail.com>
2026-04-07 18:05:07 +0000

    feat: validación de directorio

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -3,6 +3,16 @@
 DIRECTORIO=$1
 BUCKET=$2
 
-echo "Script de backup iniciado"
+if [ -z "$DIRECTORIO" ] || [ -z "$BUCKET" ]; then
+  echo "Uso: bash s3/backup_s3.sh <directorio> <bucket>"
+  exit 1
+fi
+
+if [ ! -d "$DIRECTORIO" ]; then
+  echo "Error: el directorio '$DIRECTORIO' no existe."
+  exit 1
+fi
+
+echo "Parámetros válidos"
 echo "Directorio: $DIRECTORIO"
 echo "Bucket: $BUCKET"
