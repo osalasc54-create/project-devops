e7a1eca36a1f8870066e56ae485f95edefddbaef
EC2 Default User <ec2-user@ip-172-31-17-245.ec2.internal>
2026-04-07 17:47:13 +0000

    feat: listar instancias EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -3,8 +3,21 @@
 import sys
 import boto3
 
+def listar_instancias():
+    ec2 = boto3.client("ec2")
+    response = ec2.describe_instances()
+
+    for reserva in response["Reservations"]:
+        for instancia in reserva["Instances"]:
+            print(instancia["InstanceId"], instancia["State"]["Name"])
+
 def main():
-    print("Script EC2 iniciado")
+    if len(sys.argv) < 2:
+        print("Uso: listar")
+        return
+
+    if sys.argv[1] == "listar":
+        listar_instancias()
 
 if __name__ == "__main__":
     main()
