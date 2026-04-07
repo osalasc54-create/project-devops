895657a9267a2ef70005504053615ca8eef81754
Omar Salas <osalasc54@gmail.com>
2026-04-07 18:19:00 +0000

    feat: script base deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ACCION=$1
+INSTANCE_ID=$2
+DIRECTORIO=$3
+BUCKET=$4
+
+echo "Deploy iniciado"
+echo "Acción: $ACCION"
+echo "Instancia: $INSTANCE_ID"
+echo "Directorio: $DIRECTORIO"
+echo "Bucket: $BUCKET"
